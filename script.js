1ade172898000f67b894f709aac8f0c5a0219e46
TwinHercules <dimastwin85@gmail.com>
2025-04-22 17:53:39 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
-let popup = document.getElementById("popup");
-
-function openPop() {
-    popup.classList.add("open-popup");
+function openPopup(event) {
+    event.preventDefault();
+    document.getElementById('popupModal').style.display = 'block';
 }
 
-function closePop() {
-    popup.classList.remove("open-popup");
+function closePopup() {
+    document.getElementById('popupModal').style.display = 'none';
 }
-
-<div class="card">
-<img src="img/seafood-1.jfif" alt="makanan">
-<div class="desk-menu">
-    <h3>Seafood Gurita</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa sunt sequi veniam.</p>
-    <p class="harga">Rp. 12.000</p>
-    <a href="#" onclick="openPop()">Buy</a>
-    <div class="popUp" id="popup">
-        <h2>Thanks For Order!</h2>
-        <a href="#" onclick="closePop()">Kembali</a>
-    </div>
-</div>
-</div>
